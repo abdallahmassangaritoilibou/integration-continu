9ec240757b5e1d335f398c25a6b690216ab1b110
abdallahmassangaritoilibou <abdallah.toilbou@laposte.net>
2025-03-31 14:23:56 +0200

    test réussi

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,15 @@
 const fizzbuzz = (number) => {
+    if (number % 3 === 0 && number % 5 === 0){
+        return "fizzbuzz"
+    }
+    
     if (number % 3 == 0) {
-      return "Fizz"
+      return "fizz"
     }
-    if (number === 5) {
-      return "Buzz"
+    if (number % 5 === 0 ) {
+      return "buzz"
     }
+    
     return number
   }
   
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -3,5 +3,17 @@ const assert = require('node:assert');
 const { fizzbuzz } = require('./fizzbuzz.js')
 
 test('fizzbuzz(3)', (t) => {
-  assert.strictEqual(fizzbuzz(3), "Fizz");
+  assert.strictEqual(fizzbuzz(3), "fizz");
+});
+
+test('fizzbuzz(5)',(t)=>{
+    assert.strictEqual(fizzbuzz(5), "buzz");  
+});
+
+test('fizzbuzz(15)',(t)=>{
+    assert.strictEqual(fizzbuzz(15), "fizzbuzz");
+});
+
+test('fizzbuzz(7)',(t)=>{
+    assert.strictEqual(fizzbuzz(7), 7);
 });
